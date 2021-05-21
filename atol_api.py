7a4af5d31027c4ae5d2f45fd143bbd6b25cd8ae2
fdnflm <berezin.albert@gmail.com>
2021-05-21 11:29:25 +0300

    Добавлена возможность создавать задачи, получение статуса смены + комментарии

--- atol_api.py
+++ atol_api.py
@@ -2,10 +2,18 @@ import requests
 import random
 import string
 from exceptions import *
+import time
 
 
 class AtolAPI:
     def __init__(self, host, port, cashier_name=""):
+        """
+        Инициализация экземпляра Atol API
+
+        :param host: хост сервера
+        :param port: порт сервера
+        :param cashier_name: (Опционально) Кассир
+        """
         self.host = host
         self.port = port
         self.web_url = f"http://{host}:{port}/"
@@ -17,25 +25,72 @@ class AtolAPI:
         return f"AtolAPI Instance<{self.web_url}>"
 
     def __ping_webserver(self):
-        """ Check connection to the server """
+        """ Проверка соединения с Atol сервером """
         try:
             return requests.get(self.web_url, timeout=3).status_code == 200
         except requests.exceptions.ConnectTimeout:
             return False
 
     def __gen_uuid(self, length=8):
-        """ Generate unique uuid for a task """
+        """
+        Генерация уникального ID для задачи
+
+        :param length Длина генерируемого ID
+        """
         return "".join(random.choice(string.ascii_letters) for i in range(length))
 
     def __call_api(self, method, data=None, url="requests/"):
+        """
+        API запрос на сервер
+
+        :param method: GET или POST
+        :param data: json для POST запроса, пуст если метод GET
+        :param url: (Опционально) адрес до корня API, может модифицироваться для GET запросов
+        :return: Экземпляр класса Response
+        """
         if method.lower() not in ("get", 'post'):
             raise AtolRequestError("Calling API with a non-existent method")
         if data is None and method.lower() == "post":
             raise AtolRequestError("Calling API on POST method when data is None")
         elif data is not None and method.lower() == "get":
             data = {}
-        request = requests.request(method, self.web_url + url, data=data)
+        request = requests.request(method, self.web_url + url, json=data)
         return request
 
     def __get_request_result(self, uuid):
-        return self.__call_api("GET", url=f"requests/{uuid}").text
+        """
+        Получение результата исполненной задачи
+        :param uuid: идентификатор отправленной задачи
+        :return: JSON ответ от сервера
+        """
+        return self.__call_api("GET", url=f"requests/{uuid}").json()["results"][0]
+
+    def __add_task(self, dict_data):
+        """
+        Добавляет задачу для Веб-сервера
+
+        :param dict_data: JSON с аргументами для сервера
+        :return: JSON результат задачи, либо исключение
+        """
+        json_data = {
+            "uuid": self.__gen_uuid(),
+            "request": [
+                dict_data
+            ]
+        }
+        request = self.__call_api("POST", data=json_data)
+        if request.status_code == 201:
+            time.sleep(1)
+            return self.__get_request_result(json_data["uuid"])
+        else:
+            raise AtolRequestError(f"Unsuccessful add task request: {request.text}")
+
+    def get_shift_status(self):
+        """
+        Получение состояния смены
+
+        :return: возможные варианты - 'opened', 'closed', 'expired'
+        """
+        dict_data = {"type": "getShiftStatus"}
+        task = self.__add_task(dict_data)
+        return task["result"]["shiftStatus"]["state"]
